4ac15095d4568270b2379443edb07c0a9244124e
Andres Quintero <aquintero@oru.com.co>
2019-02-22 13:55:56 -0500

    update nginx

--- nginx2/docker-entrypoint.sh
+++ nginx2/docker-entrypoint.sh
@@ -17,8 +17,8 @@ PLACEHOLDER_BACKEND_PORT="3000"
 # metadata. This allows us to dynamically configure nginx at runtime.
 #
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
-#PLACEHOLDER_VHOST="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
-PLACEHOLDER_VHOST="localhost"
+PLACEHOLDER_VHOST="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
+#PLACEHOLDER_VHOST="localhost"
 
 # Where is our default config located?
 DEFAULT_CONFIG_PATH="/etc/nginx/conf.d/default.conf"
