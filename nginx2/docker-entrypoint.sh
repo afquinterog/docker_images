a1e0308a81b712d6274e8daf73eadd4001a33e34
Andres Quintero <aquintero@oru.com.co>
2019-03-04 21:55:13 -0500

    Configure domain for application

--- nginx2/docker-entrypoint.sh
+++ nginx2/docker-entrypoint.sh
@@ -17,7 +17,8 @@ PLACEHOLDER_BACKEND_PORT="3000"
 # metadata. This allows us to dynamically configure nginx at runtime.
 #
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
-PLACEHOLDER_VHOST="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
+#PLACEHOLDER_VHOST="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
+PLACEHOLDER_VHOST="http://www.parrowbs.site"
 #PLACEHOLDER_VHOST="localhost"
 
 # Where is our default config located?
